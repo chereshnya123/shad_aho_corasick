90fd71f08a4b1aea5954e0fdbc4aff68d7a86684
chereshnya123 <nikos08012@gmail.com>
2026-06-09 11:54:40 +0300

    Pass 65 tesst

--- contest_3/A/gen_a.cpp
+++ contest_3/A/gen_a.cpp
@@ -1,60 +1,123 @@
+#include <algorithm>
 #include <iostream>
-#include <map>
+#include <ranges>
 #include <string>
+#include <unordered_map>
 #include <vector>
+// #include <format>
 
-void InplaceCountSort(
-    std::map<std::pair<int, int>, std::vector<long long int>>& labels,
-    std::vector<long long int>& suffix_labels) {
-    long long int new_label = 0;
-    for (auto& [_, suffixes] : labels) {
-        if (suffixes.empty()) {
+// void PrintDoubledSa(const std::vector<std::pair<int, int>>& doubled_sa) {
+//     for (const auto& labels: doubled_sa) {
+//         std::cout << std::format("({}, {}) ", labels.first, labels.second);
+//     }
+//     std::cout << std::endl;
+// }
+
+// void PrintInvSa(const std::vector<int>& sa) {
+//     for (auto label: sa) {
+//         std::cout << label << ' ';
+//     }
+//     std::cout << std::endl;
+// }
+
+void BuildSuffixArray(
+    std::vector<int>& suffix_array,
+    const std::unordered_map<int, std::vector<int>>& pos_by_label,
+    const int kLabelsCount) {
+    suffix_array.clear();
+    for (auto label : std::views::iota(0, kLabelsCount)) {
+        if (!pos_by_label.contains(label)) {
             continue;
         }
-        for (auto suffix_pos : suffixes) {
-            suffix_labels[suffix_pos] = new_label;
+
+        for (auto pos : pos_by_label.at(label)) {
+            suffix_array.push_back(pos);
         }
-        ++new_label;
-        suffixes.clear();
     }
 }
 
-std::vector<long long int> GetInvertedSuffixArray(
-    const std::vector<long long int>& suf_array) {
-    std::vector<long long int> inverted(suf_array.size());
-    for (auto pos = 0U; pos < suf_array.size(); ++pos) {
-        auto rank = suf_array[pos];
-        inverted[rank] = pos;
+void SquashLabels(std::vector<std::pair<int, int>>& doubled_sa,
+                  std::vector<int>& inv_suffix_array) {
+    const auto kLabelsCount =
+        std::max(27, static_cast<int>(inv_suffix_array.size()));
+    std::unordered_map<int, std::vector<int>> pos_by_label{};
+    std::vector<int> suffix_array;
+    suffix_array.reserve(kLabelsCount);
+
+    // Sort by second label
+    for (auto pos = 0U; pos < doubled_sa.size(); ++pos) {
+        auto label = doubled_sa[pos].second;
+        pos_by_label[label].push_back(pos);
+    }
+    BuildSuffixArray(suffix_array, pos_by_label, kLabelsCount);
+
+    // Sort by first label
+    pos_by_label.clear();
+    for (auto pos : suffix_array) {
+        auto label = doubled_sa[pos].first;
+        pos_by_label[label].push_back(pos);
+    }
+    BuildSuffixArray(suffix_array, pos_by_label, kLabelsCount);
+
+    auto new_rank = 0;
+    inv_suffix_array[suffix_array[0]] = new_rank;
+    for (auto i = 1U; i < suffix_array.size(); ++i) {
+        auto prev_pos = suffix_array[i - 1];
+        auto cur_pos = suffix_array[i];
+        if (doubled_sa[prev_pos] != doubled_sa[cur_pos]) {
+            ++new_rank;
+        }
+        inv_suffix_array[cur_pos] = new_rank;
+    }
+}
+
+std::vector<int> GetInvertedSuffixArray(const std::vector<int>& inv_sa) {
+    const auto kClassesCount = std::max(27UL, inv_sa.size());
+    std::vector<std::vector<int>> poses_by_rank;
+    poses_by_rank.resize(kClassesCount);
+    for (auto pos = 0U; pos < inv_sa.size(); ++pos) {
+        auto rank = inv_sa[pos];
+        poses_by_rank[rank].push_back(pos);
     }
 
-    return inverted;
+    std::vector<int> suffix_array;
+    for (const auto& poses : poses_by_rank) {
+        for (auto pos : poses) {
+            suffix_array.push_back(pos);
+        }
+    }
+
+    return suffix_array;
 }
 
 // Not really suffix array, but sorted cyclic shifts
-std::vector<long long int> GetSuffixArray(const std::string& input_str) {
-    const long long int kInputSize = input_str.size();
-    std::vector<long long int> suffix_labels;
-    std::map<std::pair<int, int>, std::vector<long long int>> labels;
-    suffix_labels.reserve(kInputSize);
+std::vector<int> GetSuffixArray(const std::string& input_str) {
+    const int kInputSize = input_str.size();
+    std::vector<int> inv_suffix_array;
+    std::vector<std::pair<int, int>> doubled_sa;
+    doubled_sa.resize(kInputSize);
+    inv_suffix_array.reserve(kInputSize);
     for (auto i = 0; i < kInputSize; ++i) {
-        suffix_labels.push_back(static_cast<int>(input_str[i] - 'a'));
+        inv_suffix_array.push_back(static_cast<int>(input_str[i] - 'a'));
     }
 
-    long long int suf_len = 1;
+    int suf_len = 1;
     while (suf_len / 2 < kInputSize) {
-        auto prev_suffix_labels = suffix_labels;
+        auto prev_suffix_labels = inv_suffix_array;
         suf_len = std::min(kInputSize, suf_len);
         for (auto i = 0; i < kInputSize; ++i) {
             auto label1 = prev_suffix_labels[i];
             auto label2 = prev_suffix_labels[(i + suf_len) % kInputSize];
-            labels[{label1, label2}].push_back(i);
+            doubled_sa[i] = {label1, label2};
         }
 
-        InplaceCountSort(labels, suffix_labels);
+        // PrintDoubledSa(doubled_sa);
+        SquashLabels(doubled_sa, inv_suffix_array);
+        // PrintInvSa(inv_suffix_array);
         suf_len *= 2;
     }
 
-    return GetInvertedSuffixArray(suffix_labels);
+    return GetInvertedSuffixArray(inv_suffix_array);
 }
 
 std::string GetBWT(const std::string& input_str) {
@@ -64,7 +127,7 @@ std::string GetBWT(const std::string& input_str) {
     std::string bwt;
     bwt.reserve(kInputSize);
     for (auto i = 0U; i < kInputSize; i++) {
-        const long long int kLastCharPos =
+        const int kLastCharPos =
             (suff_array[i] + input_str.size() - 1) % input_str.size();
         bwt.push_back(input_str[kLastCharPos]);
     }
